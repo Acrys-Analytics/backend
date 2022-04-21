ccdda6a97c1f0083391aa9cb7e2cbb640e3e91bc
LeXonJe <30697846+LeXonJe@users.noreply.github.com>
2022-04-21 23:18:20 +0200

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [process.env.DOMAIN],
+  });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(3000);
 }
